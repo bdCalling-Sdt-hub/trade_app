e239742fc1abb0efad2cbda9a811e45fef5b0198
MasumRaj <promotionbd321@gmail.com>
2024-07-25 19:40:57 +0600

    category screen completed

--- lib/view/screens/profile_screen/profile_screen.dart
+++ lib/view/screens/profile_screen/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trade_app/utils/app_strings/app_strings.dart';
+import 'package:trade_app/view/components/custom_app_bar/custom_app_bar.dart';
 import 'package:trade_app/view/components/nav_bar/nav_bar.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,10 +8,13 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+
       bottomNavigationBar: NavBar(currentIndex: 4),
 
-      appBar: AppBar(title: Text("ProfileScreen"),),
+      appBar: CustomAppBar(
+        appBarContent: AppStrings.myProfile,
+      )
     );
   }
 }
