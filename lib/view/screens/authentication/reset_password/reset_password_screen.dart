b4029d6e12eb6b2c75d91890f5294fee2ae17f9d
Nadimpg <bsmrstunadimcse@gmail.com>
2024-07-25 18:14:50 +0600

    auth done

--- lib/view/screens/authentication/reset_password/reset_password_screen.dart
+++ lib/view/screens/authentication/reset_password/reset_password_screen.dart
@@ -10,8 +10,8 @@ import 'package:trade_app/view/components/custom_text_field/custom_text_field.da
 import 'package:trade_app/view/screens/authentication/auth_controller/auth_controller.dart';
 
 class ResetPasswordScreen extends StatelessWidget {
-  const ResetPasswordScreen({super.key});
-
+    ResetPasswordScreen({super.key});
+    final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,87 +22,92 @@ class ResetPasswordScreen extends StatelessWidget {
       body: GetBuilder<AuthController>(builder: (controller) {
         return SingleChildScrollView(
           padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 44.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ///<=================================Title Text=====================================>
-              Center(
-                  child: CustomText(
-                    text: AppStrings.resetPassword.tr,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                  )),
-              CustomText(
-                text: AppStrings.setTheNewPasswordFor.tr,
-                fontWeight: FontWeight.w400,
-                maxLines: 3,
-                top: 8,
-                fontSize: 16.h,
-                bottom: 24.h,
-              ),
-              ///<==============================Password section====================================>
-              CustomText(
-                text: AppStrings.enterNewPassword.tr,
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                bottom: 16.h,
-                top: 16.h,
-              ),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ///<=================================Title Text=====================================>
+                Center(
+                    child: CustomText(
+                      text: AppStrings.resetPassword.tr,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    )),
+                CustomText(
+                  text: AppStrings.setTheNewPasswordFor.tr,
+                  fontWeight: FontWeight.w400,
+                  maxLines: 3,
+                  top: 8,
+                  fontSize: 16.h,
+                  bottom: 24.h,
+                ),
+                ///<==============================Password section====================================>
+                CustomText(
+                  text: AppStrings.enterNewPassword.tr,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  bottom: 16.h,
+                  top: 16.h,
+                ),
 
-              CustomTextField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return AppStrings.fieldCantBeEmpty.tr;
-                  } else if (value.length < 8 ||
-                      !AppStrings.passRegexp.hasMatch(value)) {
-                    return AppStrings.passwordLengthAndContain.tr;
-                  } else {
+                CustomTextField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return AppStrings.fieldCantBeEmpty.tr;
+                    } else if (value.length < 8 ||
+                        !AppStrings.passRegexp.hasMatch(value)) {
+                      return AppStrings.passwordLengthAndContain.tr;
+                    } else {
+                      return null;
+                    }
+                  },
+                  textInputAction: TextInputAction.next,
+                  isPassword: true,
+                  textEditingController: controller.newPassController,
+                  hintText: AppStrings.enterNewPassword,
+                ),
+
+                ///<==============================Confirm Password section====================================>
+                CustomText(
+                  text: AppStrings.confirmPassword,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  bottom: 16.h,
+                  top: 24.h,
+                ),
+
+                CustomTextField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return AppStrings.fieldCantBeEmpty;
+                    } else if (controller.newPassController.text !=
+                        controller.confirmPassController.text) {
+                      return AppStrings.passwordShould.tr;
+                    }
                     return null;
-                  }
-                },
-                textInputAction: TextInputAction.next,
-                isPassword: true,
-                textEditingController: controller.newPassController,
-                hintText: AppStrings.enterNewPassword,
-              ),
+                  },
+                  textEditingController: controller.confirmPassController,
+                  textInputAction: TextInputAction.done,
+                  isPassword: true,
+                  hintText: AppStrings.retypeNewPassword.tr,
+                ),
 
-              ///<==============================Confirm Password section====================================>
-              CustomText(
-                text: AppStrings.confirmPassword,
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                bottom: 16.h,
-                top: 24.h,
-              ),
+                SizedBox(
+                  height: 24.h,
+                ),
 
-              CustomTextField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return AppStrings.fieldCantBeEmpty;
-                  } else if (controller.newPassController.text !=
-                      controller.confirmPassController.text) {
-                    return AppStrings.passwordShould.tr;
-                  }
-                  return null;
-                },
-                textEditingController: controller.confirmPassController,
-                textInputAction: TextInputAction.done,
-                isPassword: true,
-                hintText: AppStrings.retypeNewPassword.tr,
-              ),
-
-              SizedBox(
-                height: 24.h,
-              ),
-
-              ///<==================================   Button ===========================>
-              CustomButton(
-                onTap: () {
-                  Get.toNamed(AppRoutes.signInScreen);
-                },
-                title: AppStrings.continues.tr,
-              ),
-            ],
+                ///<==================================   Button ===========================>
+                CustomButton(
+                  onTap: () {
+                    if(formKey.currentState!.validate()){
+                      Get.toNamed(AppRoutes.signInScreen);
+                    }
+                  },
+                  title: AppStrings.continues.tr,
+                ),
+              ],
+            ),
           ),
         );
       }),
