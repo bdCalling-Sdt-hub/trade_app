d315e78e1ea36f538eb75ca951e67163143e1023
MasumRaj <promotionbd321@gmail.com>
2024-07-25 18:10:40 +0600

    category screen completed

--- lib/utils/app_const/app_const.dart
+++ lib/utils/app_const/app_const.dart
@@ -13,7 +13,7 @@ class AppConstants {
   static var chatId = "chatID";
   static String isRememberMe = "isRememberMe";
   static String userNtr = 'https://www.india.com/wp-content/uploads/2022/03/Jr-NTR-On-RRR-Massive-Success.jpg';
-  static String map = 'https://i.sstatic.net/HILmr.png';
+  static String electronics = 'https://flare.pk/wp-content/uploads/2023/01/1-1.jpg';
   static String truck = 'https://t4.ftcdn.net/jpg/00/26/80/01/360_F_26800115_YlmErNLIVZeNZXPzUc3z4GAD1gGkVABu.jpg';
 
 
