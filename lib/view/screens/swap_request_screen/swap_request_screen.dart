1690f17145d4c2f6083b359bb10fe9be074ff409
MasumRaj <promotionbd321@gmail.com>
2024-07-26 15:12:06 +0600

    swap request completed

--- lib/view/screens/swap_request_screen/swap_request_screen.dart
+++ lib/view/screens/swap_request_screen/swap_request_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:trade_app/utils/app_colors/app_colors.dart';
+import 'package:trade_app/utils/app_const/app_const.dart';
 import 'package:trade_app/utils/app_strings/app_strings.dart';
 import 'package:trade_app/view/components/custom_app_bar/custom_app_bar.dart';
-import 'package:trade_app/view/components/custom_text/custom_text.dart';
+import 'package:trade_app/view/components/custom_swap_requests/custom_swap_requests.dart';
 
 class SwapRequestScreen extends StatelessWidget {
   const SwapRequestScreen({super.key});
@@ -23,12 +24,18 @@ class SwapRequestScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomText(
-                text: AppStrings.general.tr,
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-                color: AppColors.black500,
-                bottom: 10,
+
+              CustomSwapRequests(
+                image: AppConstants.userNtr,
+                name: 'Zahid Hossain'.tr,
+                date: '12/06/24'.tr,
+                acceptButton: 'accept'.tr,
+                rejectButton: 'reject'.tr,
+                onTap: () {
+                  print('this is -============');
+                },
+                firstProductName: 'Samsung Galaxy S22'.tr,
+                exchangeProductName: 'Sony Y1G Android TV'.tr,
               ),
             ],
           ),
