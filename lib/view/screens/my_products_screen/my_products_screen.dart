1703cb742ab0031e3f68ce41f4c8bc732106ac10
MasumRaj <promotionbd321@gmail.com>
2024-07-26 17:09:18 +0600

    my products completed

--- lib/view/screens/my_products_screen/my_products_screen.dart
+++ lib/view/screens/my_products_screen/my_products_screen.dart
@@ -5,6 +5,7 @@ import 'package:trade_app/utils/app_const/app_const.dart';
 import 'package:trade_app/utils/app_strings/app_strings.dart';
 import 'package:trade_app/view/components/custom_app_bar/custom_app_bar.dart';
 import 'package:trade_app/view/components/custom_my_products/custom_my_product.dart';
+import 'package:trade_app/view/components/custom_text/custom_text.dart';
 
 class MyProductsScreen extends StatelessWidget {
   const MyProductsScreen({super.key});
@@ -17,25 +18,39 @@ class MyProductsScreen extends StatelessWidget {
         appBarContent: AppStrings.myProducts.tr,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 12,
-            mainAxisSpacing: 12,
-            childAspectRatio: 0.75, // Adjust the aspect ratio as needed
-          ),
-          itemCount: 5,
-          physics: const PageScrollPhysics(),
-          itemBuilder: (BuildContext context, int index) {
-            return CustomMyProduct(
-              isEdit: true,
-              image: AppConstants.electronics,
-              name: 'Samsung Galaxy s22'.tr,
-              onTap: () {},
-              value: '\$825+', editOnTap: () {  },
-            );
-          },
+        padding: const EdgeInsets.symmetric(horizontal: 16,),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(
+              text: AppStrings.availableProducts.tr,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+              color: AppColors.black500,
+              bottom: 10,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                  childAspectRatio: 0.75, // Adjust the aspect ratio as needed
+                ),
+                itemCount: 5,
+                physics: const PageScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomMyProduct(
+                    isEdit: true,
+                    image: AppConstants.electronics,
+                    name: 'Samsung Galaxy s22'.tr,
+                    onTap: () {},
+                    value: '\$825+', editOnTap: () {  },
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
