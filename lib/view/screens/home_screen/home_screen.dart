22eddc061f3e385fca7fd25c1ec41609e98399e9
MasumRaj <promotionbd321@gmail.com>
2024-07-27 12:27:24 +0600

    some changes

--- lib/view/screens/home_screen/home_screen.dart
+++ lib/view/screens/home_screen/home_screen.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 import 'package:trade_app/utils/app_colors/app_colors.dart';
-import 'package:trade_app/utils/app_icons/app_icons.dart';
-import 'package:trade_app/utils/app_images/app_images.dart';
-import 'package:trade_app/utils/app_strings/app_strings.dart';
-import 'package:trade_app/view/components/custom_image/custom_image.dart';
-import 'package:trade_app/view/components/custom_text/custom_text.dart';
-import 'package:trade_app/view/components/custom_text_field/custom_text_field.dart';
 import 'package:trade_app/view/components/nav_bar/nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,11 +9,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavBar(currentIndex: 0),
+      bottomNavigationBar: const NavBar(currentIndex: 0),
       backgroundColor: AppColors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 44.h,horizontal: 20.w),
-        child: Column(
+        child: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
 
