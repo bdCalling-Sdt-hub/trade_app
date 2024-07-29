eecb64ccf9a28c5c04511d164c36304e84c33e78
MasumRaj <promotionbd321@gmail.com>
2024-07-29 14:01:00 +0600

    language add

--- lib/view/screens/change_language/english.dart
+++ lib/view/screens/change_language/english.dart
@@ -1,3 +1,9 @@
 Map<String, String> english = {
   "Language":"Language",
+  "Account Info":"Account Info",
+  "Change Password":"Change Password",
+  "Address":"Address",
+  "Terms And Condition":"Terms And Condition",
+  "privacy Policy":"privacy Policy",
+  "Help Center":"Help Center",
 };
