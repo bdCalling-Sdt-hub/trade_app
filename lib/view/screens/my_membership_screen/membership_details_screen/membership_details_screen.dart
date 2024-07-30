fb787eefa7ca294fa55da27b7ab02256bfe2fe0d
MasumRaj <promotionbd321@gmail.com>
2024-07-30 11:02:08 +0600

    membership screen completed

--- lib/view/screens/my_membership_screen/membership_details_screen/membership_details_screen.dart
+++ lib/view/screens/my_membership_screen/membership_details_screen/membership_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:trade_app/utils/app_colors/app_colors.dart';
 import 'package:trade_app/view/components/custom_app_bar/custom_app_bar.dart';
 
 class MembershipDetailsScreen extends StatelessWidget {
@@ -8,6 +9,7 @@ class MembershipDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.white,
       ///======================Details Appbar==================
       appBar: CustomAppBar(appBarContent: 'Membership Details'.tr,),
     );
