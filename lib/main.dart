22c16aa8ba4c58988e4909c0b4af8f695bab5dc0
moury12 <tanzibamouri00@gmail.com>
2024-12-20 11:51:50 +0600

    ios setup incomplete

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:hive/hive.dart';
 import 'package:trade_app/core/dependency/dependency_injection.dart';
 import 'package:trade_app/core/routes/routes.dart';
 import 'package:trade_app/dependency_injection/path.dart';
 import 'package:trade_app/service/socket_service.dart';
 import 'package:trade_app/view/components/device_utils/device_utils.dart';
-import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,18 +19,10 @@ Future<void> main() async {
   Stripe.publishableKey =
       "pk_test_51Q69G2L8xn5o73J1fDJ9iprBk2h3Kjbj1zXs1Rll89x1qFFXPAtHqyZ667hyVpAIlUvIJRzzhDXmRxQ6r9tOeOlZ00mZDAdEoI";
 
-  /// ================= DB Path ===============
-  var databasePath = await getApplicationDocumentsDirectory();
-  Hive.init(databasePath.path);
-  // ================ Open the 'users' box before using it ===============
-
-  await Hive.openBox('users');
-
   initDependencies();
   runApp(
     const MyApp(), // Wrap your app
   );
-  // const MyApp());
 }
 
 class MyApp extends StatelessWidget {
